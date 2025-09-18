c4dbad1ac424f994e99df4478795189ab891629d
mirontypo <mirontypo@gmail.com>
2025-09-18 03:12:22 +0300

    Refactoring + ALOT OF FIXES

--- bossgame.py
+++ bossgame.py
@@ -5,7 +5,7 @@ import arcade
 
 clear = lambda: os.system('cls')
 
-#LOX 2
+#LOX 3
 
 #Считывание и проверка длины имени игрока
 playername = input("Введи своё имя: ")
@@ -13,11 +13,12 @@ while len(playername) > 5 or len(playername) < 3:
     print("Размер имени от 3 до 5 символов...")
     playername = input("Введи своё имя: ")
 
-def refteshscreen_time():
+#Функция очистки
+def refreshscreen_time():
     time.sleep(1.35)
     clear()
 
-
+#ACSII
 
 drawmain = fr"""
 ======================================
@@ -110,13 +111,15 @@ drawtemplate = """
 
 
 
-
+#Переменные
 playerhp = int(20)
 playermana = int(0)
 bosshp = int(20)
+
 fire_dot_damage = -1
 freezebuildup = -1
 
+#Звуки
 swordsound = arcade.load_sound("sounds/sword.wav")
 healsound = arcade.load_sound("sounds/heal.wav")
 bossattack = arcade.load_sound("sounds/bossattack.wav")
@@ -125,127 +128,149 @@ bossattack = arcade.load_sound("sounds/bossattack.wav")
 #if playername == "Аллах":
     #playerhp = int(999)
 
+#Основной цикл
 while True:
-    #Переменные урона и хила вынес в начало цикла
+    #Переменные урона, хилла и скиллов
     pdamage = random.randint(1, 3)
     playerheal = random.randint(1, 3)
-    fireballdamage = random.randint(2, 6)
-    icesharddamage = random.randint(1, 3)
+    fireballdamage = random.randint(2, 4)
+    icesharddamage = random.randint(1, 2)
+
+    #Переменные для босса
+    bdamage = random.randint(1, 3)
 
     #Проверка на смерть босса
     if bosshp < 1:
-        refteshscreen_time()
+        refreshscreen_time()
         print(drawbossdead)
         print(f"Ты убил босса!")
         exit()
 
-    refteshscreen_time()
+    #Главный экран
+    refreshscreen_time()
     print(drawmain)
 
-    #логика игрока
 
-    action = int(input(f"Твоё хп: {playerhp} | Твоя мана: {playermana} | Хп Босса: {bosshp}\n [1] чтобы нанести урон! [2] Восстановить здоровье! [3] Меню выбора навыков!"))
+    #Логика игрока
+    action = int(input(f"Твоё хп: {playerhp} | Твоя мана: {playermana} | Хп Босса: {bosshp}\n [1] Ударить мечом! [2] Восстановить здоровье! [3] Меню выбора навыков!"))
+
+    #Удар мечом
     if action == 1:
-
-        refteshscreen_time()
-
+        refreshscreen_time()
         print(drawsword)
-
         arcade.play_sound(swordsound)
 
         playermana += pdamage
         bosshp -= pdamage
+
         print(f"{playername} нанёс {pdamage} урона! Здоровье Гриши: {bosshp}")
 
-
+    #Хилка
     elif action == 2:
-
-
         if playerhp >= 20:
             playerhp -= playerheal
             print(f"Еблан? У тебя фулл хп. Лося за втык! Хилка нанесла {playerheal} урона! Текущее здоровье {playerhp}")
 
-
-
-
         else:
-             refteshscreen_time()
-
+             refreshscreen_time()
              arcade.play_sound(healsound)
 
              playerhp = min(playerheal + playerhp, 20)
 
              print(drawheal)
-
              print(f"Хилка дала {playerheal} Текущее здоровье {playerhp} ")
 
 
-    #меню выбора навыков
+    #Меню выбора навыков
     elif action == 3:
         skillchoise = int(input(f"\nДоступные навыки:\n[1] Фаерболл! [2] Ледяной осколок! [3] Вернуться назад!"))
 
-
-        #выбор фаербола
+        #Выбор фаербола
         if skillchoise == 1:
-            refteshscreen_time()
+            if playermana >= 3:
 
-            print(drawfireballsucces)
+                refreshscreen_time()
+                print(drawfireballsucces)
 
-            bosshp -= fireballdamage
-            playermana -= 3
+                bosshp -= fireballdamage
+                playermana -= 3
 
-            print (f"Нанесенно: {fireballdamage} Здоровье босса: {bosshp}")
+                print (f"Нанесенно: {fireballdamage} Здоровье босса: {bosshp}")
 
             #шанс прока дот урона от огня
-            if random.random() < 0.5:
-                refteshscreen_time()
-                fire_dot_damage = 3
-                print(drawtemplate)
-                print(f"Босс загорелся на 3 хода!")
-        #если не хватает маны
-        elif skillchoise == 1 and playermana <3:
-            refteshscreen_time()
-            playerhp -= 1
-            print(drawfireballfailed)
-            print (f"Фаерболл взорвался в руке и нанёс 1 урона! Текущее здоровье: {playerhp}")
+                if random.random() < 0.5:
+
+                    refreshscreen_time()
+                    fire_dot_damage = 3
+
+                    print(drawtemplate)
+                    print(f"Босс загорелся на 3 хода!")
+        #Если не хватает маны
+            else:
+                refreshscreen_time()
+
+                playerhp -= 1
+
+                print(drawfireballfailed)
+                print (f"Фаерболл взорвался в руке и нанёс 1 урона! Текущее здоровье: {playerhp}")
 
 
-        #ледяной урон
+        #Ледяной урон
         elif skillchoise == 2:
-            refteshscreen_time()
-            playermana -= 3
-            bosshp -= icesharddamage
-            print(drawtemplate)
-            print(f"Ледяной осколок нанёс {icesharddamage} Здоровье босса: {bosshp}")
+            if playermana >= 3:
+                refreshscreen_time()
 
-            if random.random() < 0.5:
-                freezebuildup = 2
+                playermana -= 3
+                bosshp -= icesharddamage
+
+                print(drawtemplate)
+                print(f"Ледяной осколок нанёс {icesharddamage} Здоровье босса: {bosshp}")
+
+                if random.random() < 0.5:
+                    freezebuildup = 2
+
+            #Если не хватает маны
+            else:
+                playerhp -= 1
+
+                print(drawtemplate)
+                print(f"Ледяной осколок обморозил руку и нанёс 1 урона! Текущее здоровье: {playerhp} ")
+
+        #Вернуться назад
         elif skillchoise == 3:
             continue
 
-    #логика босса
+
+
+    #Проверка Босса на Фриз
     if freezebuildup > 0:
-        refteshscreen_time()
+        refreshscreen_time()
+
+        freezebuildup -= 1
+
         print(drawtemplate)
         print(f"Босс заморожен на {freezebuildup} хода!")
-        freezebuildup -= 1
+
+
+    #Логика босса
     else:
-        refteshscreen_time()
-
+        refreshscreen_time()
         print(drawbossattack)
-
         arcade.play_sound(bossattack)
 
         bdamage = random.randint(1, 3)
         playerhp -= bdamage
+
         print (f"Босс Гриша нанёс {bdamage} урона!")
 
 
     #Счётчик дот урона от фаерболла
     if fire_dot_damage >= 0:
-        refteshscreen_time()
+        refreshscreen_time()
+
         fire_dot_damage -= 1
         bosshp -= fire_dot_damage
+
         print(drawtemplate)
         print("Босс получает 1 урон от огня")
 
